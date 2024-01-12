9d20de3fed9cf905ee41a563279346b1a55074cb
shadowgks <saad.moumou.coder@gmail.com>
2024-01-12 09:07:08 +0100

    build structure the project

---
+++ src/main/java/com/example/vistreamv2/Vistreamv2Application.java
@@ -0,0 +1,13 @@
+package com.example.vistreamv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vistreamv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vistreamv2Application.class, args);
+	}
+
+}
