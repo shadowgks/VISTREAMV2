79b94ac35685e6dea0849ab14edf9d5381a2a206
shadowgks <saad.moumou.coder@gmail.com>
2024-03-21 22:34:59 +0000

    Implemented filtering movies by genre in GenreRepository

--- src/main/java/com/example/vistreamv2/repositories/GenreRepository.java
+++ src/main/java/com/example/vistreamv2/repositories/GenreRepository.java
@@ -4,9 +4,11 @@ import com.example.vistreamv2.models.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
     Genre findGenreByName(String name);
+    List<Genre> findAllByOrderByNameAsc();
     Optional<Genre> findGenreByIdTmdb(Long id);
 }
