eb0a0b22c14c63126e1470ef9745852768f95355
shadowgks <saad.moumou.coder@gmail.com>
2024-03-26 15:47:40 +0000

    Modify CreditReqDto.java

--- src/main/java/com/example/vistreamv2/dtos/requests/credit/CreditReqDto.java
+++ src/main/java/com/example/vistreamv2/dtos/requests/credit/CreditReqDto.java
@@ -1,9 +1,15 @@
 package com.example.vistreamv2.dtos.requests.credit;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter @Setter
+@Builder
 public class CreditReqDto {
     private Long idTmdb;
     private Boolean adult;
     private Integer gender;
+    @NotNull(message = "name must not be null")
     private String name;
     private Double popularity;
     private String profilePath;
