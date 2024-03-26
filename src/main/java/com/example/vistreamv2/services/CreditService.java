2ed85f0a61766b042e04e096e8eedc580d254898
shadowgks <saad.moumou.coder@gmail.com>
2024-03-26 15:47:41 +0000

    Modify CreditService.java and CreditServiceImpl.java

--- src/main/java/com/example/vistreamv2/services/CreditService.java
+++ src/main/java/com/example/vistreamv2/services/CreditService.java
@@ -5,7 +5,11 @@ import com.example.vistreamv2.models.entity.Media;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface CreditService {
     Page<Credit> findAllCreditPageable(String searchTerm, Pageable pageable);
-
+    void savedCredits(List<Credit> credits);
+    Credit updateCredit(Credit credit, Long id);
+    void deleteCredit(Long id);
 }
--- src/main/java/com/example/vistreamv2/services/impls/CreditServiceImpl.java
+++ src/main/java/com/example/vistreamv2/services/impls/CreditServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CreditServiceImpl implements CreditService {
@@ -18,5 +20,37 @@ public class CreditServiceImpl implements CreditService {
     public Page<Credit> findAllCreditPageable(String searchTerm, Pageable pageable) {
         return creditRepository.findMediaBySearch(searchTerm, pageable)
                 .orElseThrow(() -> new IllegalArgumentException("Not found credits"));
+    }
+
+    @Override
+    public void savedCredits(List<Credit> credits) {
+        credits.forEach(c -> checkCreditIdTmdb(c.getIdTmdb()));
+        creditRepository.saveAll(credits);
+    }
+
+    @Override
+    public Credit updateCredit(Credit credit, Long id) {
+        Credit findCredit = creditRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Not found this credit: "+ id));
+        // Set all properties of findCredit
+        findCredit.setIdTmdb(findCredit.getIdTmdb());
+        findCredit.setAdult(credit.getAdult());
+        findCredit.setGender(credit.getGender());
+        findCredit.setName(credit.getName());
+        findCredit.setPopularity(credit.getPopularity());
+        findCredit.setProfilePath(credit.getProfilePath());
+        return creditRepository.save(findCredit);
+    }
+
+    @Override
+    public void deleteCredit(Long id) {
+        Credit credit = creditRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Not found this credit: "+ id));
+        creditRepository.delete(credit);
+    }
+
+    public void checkCreditIdTmdb(Long idTmdb){
+        creditRepository.findCreditByIdTmdb(idTmdb)
+                .orElseThrow(() -> new IllegalArgumentException("This id "+ idTmdb +" Tmdb already exist"));
     }
 }
