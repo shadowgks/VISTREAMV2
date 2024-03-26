d55abfb53388f3d08e737ea680b83a1b7ae520ec
shadowgks <saad.moumou.coder@gmail.com>
2024-03-26 15:46:26 +0000

    Add MeResourceResDTO.java

--- src/main/java/com/example/vistreamv2/dtos/response/user/MeResourceResDTO.java
+++ src/main/java/com/example/vistreamv2/dtos/response/user/MeResourceResDTO.java
@@ -1,2 +1,26 @@
-package com.example.vistreamv2.dtos.response.user;public class MeResourceDtoRes {
+package com.example.vistreamv2.dtos.response.user;
+
+import com.example.vistreamv2.dtos.response.role.RoleResDto;
+import com.example.vistreamv2.models.entity.PermissionGroup;
+import com.example.vistreamv2.models.entity.Role;
+import com.example.vistreamv2.models.entity.Watchlist;
+import jakarta.persistence.Column;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Set;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MeResourceResDTO {
+    String firstName;
+    String lastName;
+    String userNamee;
+    String email;
+    LocalDateTime accessionDate;
+    Set<RoleResDto> roles;
 }
