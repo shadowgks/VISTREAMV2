87920978d78b89ab1125093655079442b04df94a
shadowgks <saad.moumou.coder@gmail.com>
2024-03-26 15:46:26 +0000

    Add RoleResDto.java

--- src/main/java/com/example/vistreamv2/dtos/response/role/RoleResDto.java
+++ src/main/java/com/example/vistreamv2/dtos/response/role/RoleResDto.java
@@ -1,2 +1,17 @@
-package com.example.vistreamv2.dtos.response.role;public class RoleResDto {
+package com.example.vistreamv2.dtos.response.role;
+
+import com.example.vistreamv2.dtos.response.permission.PermissionResDto;
+import com.example.vistreamv2.models.entity.Permission;
+import lombok.*;
+
+import java.util.Set;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoleResDto {
+    String name;
+    Set<PermissionResDto> permissions;
 }
